bd6cf6922ca44af1cc87915dec05c6b20aa693c2
Lohith701 <146410468+Lohith701@users.noreply.github.com>
2025-08-21 23:11:20 +0530

    Update mini.py

--- mini.py
+++ mini.py
@@ -132,8 +132,9 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release video capture and close windows..
+# Release video capture and close windows
 cap.release()
 cv2.destroyAllWindows()
+
 
 
